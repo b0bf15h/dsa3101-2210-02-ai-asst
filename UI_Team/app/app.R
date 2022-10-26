59043f879afe688a567b4e4ea3083939f21e88c3
Bryan Lee <bleejins@gmail.com>
2022-10-26 22:39:40 +0800

    updated UI

--- UI_Team/app/app.R
+++ UI_Team/app/app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(tidyverse)
 library(plotly)
@@ -14,11 +5,15 @@ library(httr)
 library(jsonlite)
 library(RColorBrewer)
 library(renv)
+library(bslib)
+
 renv::init(bare=TRUE)
 renv:snapshot()
 flask_url <- "http://flask:5000/"
 
 ui <- fluidPage(
+  
+  theme = bs_theme(version = 4, booswatch = "minty"),
   
   #Application title
   titlePanel("AI Assistant for Medical Sales Representative"),
@@ -38,12 +33,13 @@ ui <- fluidPage(
     
     mainPanel(
       tabsetPanel(
-        tabPanel(
-          "Description",
-          uiOutput("help_text"),
-          plotOutput("distPlot")
-        ),
+        id="inTabset", type = 'hidden',
+        tabPanel("Description",
+                 uiOutput("help_text"),
+                 plotOutput("distPlot")),
         
+        tabPanel("Benefits",
+                 tableOutput("newTable"))
       )
       
     )
@@ -52,17 +48,33 @@ ui <- fluidPage(
   
 )
 
-server <- function(input,output){
+server <- function(input,output, session){
+  #action button for "What?" -> Description Tab
+  observeEvent(input$what.button, {
+    updateTabsetPanel(session, "inTabset",
+                      selected = "Description")
+  })
+  
+  #action button for "Why?" -> Benefits Tab  
+  observeEvent(input$why.button, {
+    updateTabsetPanel(session, "inTabset",
+                      selected = "Benefits")
+  })
+  
   
   output$help_text <- renderUI({
     HTML("<b> Click 'Show plot' to show the plot. </b>")
   })
   
+  #output Description for Watchman
   output$distPlot <- renderPlot({
     plot_data()
   })
   
-  
+  #Output Benefits for Watchman
+  output$newTable <- renderTable({
+    data()
+  })
   
 }
 
