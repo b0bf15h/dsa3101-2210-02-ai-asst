7663c33a899906f2307c10f8e5f7db50dd3a4c66
Bryan Lee <bleejins@gmail.com>
2022-10-27 17:19:04 +0800

    Updated app.R

--- UI_Team/app/app.R
+++ UI_Team/app/app.R
@@ -1,32 +1,55 @@
-
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-
+library(tidyverse)
 library(plotly)
 library(httr)
 library(jsonlite)
 library(RColorBrewer)
-
 library(bslib)
+
+library(shinyjs)
+library(DT)
 
 
 flask_url <- "http://flask:5000/"
+model <- jsonlite::fromJSON("test_model.js")
+
+# If no input from user, use default model
+default_model <- c(
+  "Very interesting",
+  "I am not sure I understand you fully",
+  "What does that suggest to you?",
+  "Please continue",
+  "Go on",
+  "Do you feel strongly about discussing such things?"
+)
+
+
+# Function to generate response
+Chatbot <- function(input) {
+  # match keywords from model
+  pos <- which(lapply(paste0("(.*)?", names(model), "(.*)?"), grep, x = input, ignore.case = TRUE) == 1)
+  output <- unlist(model[pos])
+  if (length(pos) == 0) {
+    # choose default answer randomly if no keyword is found
+    output <- sample(default_model, 1)
+  } else {
+    # choose applicable answer randomly
+    pos <- ifelse(length (pos) > 1, sample(pos, 1), pos)
+    output <- sample(output, 1)
+    names(output) <- NULL
+    # customize answer
+    tmp <- regexec(names(model)[pos], input, ignore.case = TRUE)[[1]]
+    end_phrase <- substr(input, start = attr(tmp, "match.length") + as.numeric(tmp) + 1, stop = nchar(input))
+    end_phrase <- trimws(end_phrase, which = "right", whitespace = "[?!.]")
+    output <- sub("\\$", end_phrase, output)
+  }
+  output
+}
 
 ui <- fluidPage(
-  
   theme = bs_theme(version = 4, booswatch = "minty"),
-  
   #Application title
   titlePanel("AI Assistant for Medical Sales Representative"),
-  
   #Sidebar for information of product
   sidebarLayout(
     sidebarPanel(
@@ -52,13 +75,25 @@ ui <- fluidPage(
       )
       
     )
-    
-  )
+  ),
   
+  fluidRow(
+    column(3, style = "position: absolute; bottom: 5px; left: 0 ",
+           wellPanel(
+             tags$div(id = 'placeholder', style = "max-height: 200px; overflow: auto"),
+             div(id = 'txt_label', textInput('txt', 'How can I help you?'),
+                 actionButton('insertBtn', 'Insert'),
+                 actionButton('removeBtn', 'Remove'),
+                 actionButton('clearBtn', 'Clear')),
+             tags$style(type="text/css", "#txt_label {font-weight: bold; font-size: 25px}")
+           ),
+           offset = 9
+    )
+  )
 )
 
-server <- function(input,output, session){
-  #action button for "What?" -> Description Tab
+server <- function(input,output,session){
+  
   observeEvent(input$what.button, {
     updateTabsetPanel(session, "inTabset",
                       selected = "Description")
@@ -85,8 +120,45 @@ server <- function(input,output, session){
     data()
   })
   
+  # Chatbot
+  inserted <- c()
+  
+  observeEvent(input$insertBtn, {
+    btn <- input$insertBtn
+    id <- paste0('txt', btn)
+    value <- input$txt
+    insertUI(
+      selector = '#placeholder',
+      ## wrap element in a div with id for ease of removal
+      ui = tags$div(
+        # tags$p(paste('Number: ', btn)),
+        tags$p(paste('You: ', value)),
+        tags$p(renderText({paste("Chatbot: ", Chatbot(value))})),
+        id = id
+      )
+    )
+    updateTextInput(session, "txt", value = "")
+    inserted <<- c(id, inserted)
+  })
+  
+  observeEvent(input$removeBtn, {
+    removeUI(
+      ## pass in appropriate div id
+      selector = paste0('#', inserted[1])
+    )
+    inserted <<- inserted[-1]
+  })
+  
+  observeEvent(input$clearBtn, {
+    removeUI(
+      ## pass in appropriate div id
+      selector = paste0('#', inserted),
+      multiple = T
+    )
+    inserted <<- c()
+  })
+  
 }
-
 
 #Run the application
 shinyApp(ui = ui, server = server)
