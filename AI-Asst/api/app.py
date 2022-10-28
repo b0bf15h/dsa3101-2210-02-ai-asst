86f1d4fa1d6f67d9f1dab283388ca74fc274c8eb
junweilow <lowjunwei99@gmail.com>
2022-10-28 15:06:52 +0800

    remove setup

--- AI-Asst/api/app.py
+++ AI-Asst/api/app.py
@@ -9,38 +9,6 @@ host = os.environ.get("ELASTICSEARCH_HOST", "localhost")
 document_store = ElasticsearchDocumentStore(host=host, username="", password="", index="document")
 
 
-
-
-from haystack.utils import convert_files_to_docs
-from haystack.nodes import PreProcessor
-
-devices = os.listdir("../../Modelling_Team/Datasets")
-
-for device in devices:
-    doc_dir = os.path.join("../../Modelling_Team/Datasets", device)
-
-    all_docs = convert_files_to_docs(dir_path=doc_dir, clean_func = lambda x:x.replace("-\n", "").replace("\n"," "), split_paragraphs=True)
-
-    for doc in all_docs:
-        doc.meta['device'] = device
-
-    preprocessor = PreProcessor(
-        clean_empty_lines=True,
-        clean_whitespace=True,
-        clean_header_footer=False,
-        split_by="word",
-        split_length=200,
-        split_overlap=30,
-        split_respect_sentence_boundary=True,
-        add_page_number=True
-    )
-    docs = preprocessor.process(all_docs)
-
-    document_store.write_documents(docs)
-
-
-
-
 from haystack.nodes import BM25Retriever
 
 retriever = BM25Retriever(document_store=document_store)
