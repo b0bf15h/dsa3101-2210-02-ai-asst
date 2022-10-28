ce616330f02a231abda86a9bc0afe4ebe7ab679a
ruijue wang <ruijuewang@ruijuedeMacBook-Pro.local>
2022-10-28 22:47:19 +0800

    update chatbot

--- UI_Team/app/app.R
+++ UI_Team/app/app.R
@@ -18,44 +18,14 @@ library(DT)
 
 
 flask_url <- "http://flask:5000/"
-model <- jsonlite::fromJSON("test_model.js")
-
-# If no input from user, use default model
-default_model <- c(
-  "Very interesting",
-  "I am not sure I understand you fully",
-  "What does that suggest to you?",
-  "Please continue",
-  "Go on",
-  "Do you feel strongly about discussing such things?"
-)
 
 
-# Function to generate response
-Chatbot <- function(input) {
-  # match keywords from model
-  pos <- which(lapply(paste0("(.*)?", names(model), "(.*)?"), grep, x = input, ignore.case = TRUE) == 1)
-  output <- unlist(model[pos])
-  if (length(pos) == 0) {
-    # choose default answer randomly if no keyword is found
-    output <- sample(default_model, 1)
-  } else {
-    # choose applicable answer randomly
-    pos <- ifelse(length (pos) > 1, sample(pos, 1), pos)
-    output <- sample(output, 1)
-    names(output) <- NULL
-    # customize answer
-    tmp <- regexec(names(model)[pos], input, ignore.case = TRUE)[[1]]
-    end_phrase <- substr(input, start = attr(tmp, "match.length") + as.numeric(tmp) + 1, stop = nchar(input))
-    end_phrase <- trimws(end_phrase, which = "right", whitespace = "[?!.]")
-    output <- sub("\\$", end_phrase, output)
-  }
-  output
-}
-
+#Chatbot function
+source("chatbot.R",local=T)
 productlist <- c('Watchman', 'Atriclip', 'Lariat')
 
 ui <- fluidPage(
+  useShinyjs(),
   theme = bs_theme(version = 4, booswatch = "minty"),
   #Application title
   titlePanel("AI Assistant for Medical Sales Representative"),
@@ -95,17 +65,24 @@ ui <- fluidPage(
   hr(),
   
   fluidRow(
-    column(3, style = "position: absolute; bottom: 5px; left: 0 ",
-           wellPanel(
-             tags$div(id = 'placeholder', style = "max-height: 200px; overflow: auto"),
-             div(id = 'txt_label', textInput('txt', 'How can I help you?'),
-                 actionButton('insertBtn', 'Insert'),
-                 actionButton('removeBtn', 'Remove'),
-                 actionButton('clearBtn', 'Clear')),
-             tags$style(type="text/css", "#txt_label {font-weight: bold; font-size: 25px}")
-           ),
-           offset = 9
-    )
+  column(3, style = "position: absolute; bottom: 5px; left: 0 ",
+         div(actionButton("chatbotBtn", "Chatbot", style="font-size:30px"), 
+             style="right:10px; bottom: 5px; position: absolute"),
+    
+    hidden(wellPanel(id = 'chat', style="bottom:70px",
+    tags$div(id = 'placeholder', style = "max-height: 200px; overflow: auto"),
+    hidden(tags$div(id = "else", actionButton("elseBtn","Show me the something else", style ="color: red; font-size: 13px" ),
+                    actionButton("switchBtn","Switch to another device",  style ="color: red;font-size: 13px"))),
+    div(id = 'txt_label', textInput('txt', 'How can I help you?', placeholder = "Enter your questions"),
+    actionButton('insertBtn', 'Insert'),
+    actionButton('removeBtn', 'Remove'),
+    actionButton('clearBtn', 'Clear')),
+    tags$br(),
+    
+    tags$style(type="text/css", "#txt_label {font-weight: bold; font-size: 25px}")
+    )),
+  offset = 9
+  )
   )
 )
 
@@ -138,41 +115,140 @@ server <- function(input,output,session){
   })
   
   # Chatbot
+  observeEvent(input$chatbotBtn, {
+    toggle("chat")
+    insertUI(
+      selector = '#placeholder',
+      ui = tags$div(
+        tags$p(renderText({paste("Chatbot: ","Hello, I am your AI Assistant!")})),
+        tags$p(renderText({paste("Chatbot: ","What device are you looking for?")}))
+      )
+    )
+  })
+  
   inserted <- c()
+  device <- c()
+  ques <- c()
+  btn <- c()
   
   observeEvent(input$insertBtn, {
-    btn <- input$insertBtn
+    if (length(btn)==0){btn <<- input$insertBtn}
+    else btn <<- btn+1
     id <- paste0('txt', btn)
-    value <- input$txt
+    text <- input$txt
+    insertUI(
+      selector = '#placeholder',
+      ui = tags$div(
+        tags$b(paste('You: ', text)),
+        if(text!=""){
+        # check whether previous device inserted
+        if (length(device) == 0) {found <- check_device(text, productlist)}
+        else {found <- -1} # Have revious device
+        
+        if(found==0){
+          # Not valid device
+          tags$div(tags$p(renderText({paste("Chatbot: ", "Sorry we do not have this device infomation!")})),
+                    tags$p(renderText({paste("Chatbot: Please search for another device!")})),
+                            tags$hr())
+          }else if(found != -1){
+            # A valid device found 
+            device <<- c(text, device)
+            tags$p(renderText({paste("Chatbot: ", "Great! What is your question?")}))
+        }else{
+          # Text is the question (after searching for a valid device)
+          ques <<- c(text, ques)
+          answer <- build_chatbot(device, ques, find=1)
+          tags$p(renderText({paste("Chatbot:[", device[length(device)], "]", answer)}))
+        }
+        }else{
+          tags$p(renderText({paste("Chatbot: ", "I am not sure I understand you fully")}))
+        },
+        id = id
+      )
+    )
+    if(text!="" && found == -1){
+      show("else")
+    }else{hide("else")}
+    
+    updateTextInput(session, "txt",  value= "")
+    inserted <<- c(id, inserted)
+  })
+  
+  observeEvent(input$elseBtn,{
+    btn <<- btn + 1
+    id <- paste0('txt', btn)
+    answer <- build_chatbot(device, ques[1], input$elseBtn+1)
     insertUI(
       selector = '#placeholder',
       ## wrap element in a div with id for ease of removal
       ui = tags$div(
-        # tags$p(paste('Number: ', btn)),
-        tags$p(paste('You: ', value)),
-        tags$p(renderText({paste("Chatbot: ", Chatbot(value))})),
+        tags$b(paste('You: ','Show me something else about "', ques[1], '"')),
+        tags$p(renderText({paste("Chatbot:[", device[length(device)], "]", answer)})),
         id = id
       )
     )
-    updateTextInput(session, "txt", value = "")
+    
+    if (answer == "Sorry, no more infomation available.  Please search for another device!"){
+      hide("else")
+    }
+    
+    ques <<- c(ques[1], ques)
+    inserted <<- c(id, inserted)
+  })
+  
+  observeEvent(input$switchBtn,{
+    btn <<- btn + 1
+    id <- paste0('txt', btn)
+    insertUI(
+      selector = '#placeholder',
+      ui = tags$div(
+        tags$hr(),
+        tags$b(renderText({paste("Chatbot: ","What device are you looking for?")})),
+        tags$br(),
+        device <<- c(),
+        ques <<- c(),
+        id = id
+      )
+    )
+    hide("else")
     inserted <<- c(id, inserted)
   })
   
   observeEvent(input$removeBtn, {
+    btn <<- btn + 1
+    id <- paste0('txt', btn)
     removeUI(
-      ## pass in appropriate div id
-      selector = paste0('#', inserted[1])
+      selector = paste0('#', inserted[1]),
     )
+    hide("else")
     inserted <<- inserted[-1]
+    
+    insertUI(
+      selector = paste0('#', inserted[1]),
+      ui = tags$div(
+        if (length(ques) == 0){
+          device <<- c()
+          tags$b(renderText({paste("Chatbot: ","Please type the device you want to search for")}))
+          tags$br()
+        }else{
+          ques <<- ques[-1]
+          tags$b(renderText({paste('Chatbot: ', 'You can also ask questions about "', 
+                                   device[length(device)], '"')}))
+          tags$br()
+        }
+      )
+    )
   })
   
   observeEvent(input$clearBtn, {
     removeUI(
-      ## pass in appropriate div id
       selector = paste0('#', inserted),
       multiple = T
     )
+    hide("else")
     inserted <<- c()
+    device <<- c()
+    ques <<- c()
   })
   
 }
