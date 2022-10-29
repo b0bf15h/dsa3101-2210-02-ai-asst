1d1480b4476868ecf401801dccf11434fe2938ab
junweilow <lowjunwei99@gmail.com>
2022-10-29 19:19:04 +0800

    Changed host

--- AI-Asst/api/app.py
+++ AI-Asst/api/app.py
@@ -4,6 +4,9 @@ import os
 from haystack.document_stores import ElasticsearchDocumentStore
 from flask import Flask, request, jsonify, send_file, render_template
 
+import time
+time.sleep(30)
+
 # Get the host where Elasticsearch is running, default to localhost
 host = os.environ.get("ELASTICSEARCH_HOST", "localhost")
 document_store = ElasticsearchDocumentStore(host=host, username="", password="", index="document")
