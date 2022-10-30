840297c385f2c5be007e7f741238c04742990e6e
junweilow <lowjunwei99@gmail.com>
2022-10-30 19:43:37 +0800

    refactored into function

--- Modelling_Team/testing_api.py
+++ Modelling_Team/testing_api.py
@@ -1,18 +1,23 @@
 import requests
 
-URL = "http://localhost:5000/prediction"
+def test_api(URL, question, device):
 
-question = "What is the dimension of the Watchman device"
-device = "Watchman"
-  
-# defining a params dict for the parameters to be sent to the API
-PARAMS = {'question':question, "device":device}
-  
-# sending get request and saving the response as response object
-r = requests.get(url = URL, params = PARAMS)
-  
-# extracting data in json format
-data = r.json()
-  
-  
-print(data)
+    #URL = "http://localhost:5000/prediction"
+
+    #question = "What is the dimension of the Watchman device"
+    #device = "Watchman"
+    
+    # defining a params dict for the parameters to be sent to the API
+    PARAMS = {'question':question, "device":device}
+    
+    # sending get request and saving the response as response object
+    r = requests.get(url = URL, params = PARAMS)
+    
+    # extracting data in json format
+    data = r.json()
+    
+    
+    return data
+
+
+# test_api("http://localhost:5000/prediction", "What is the dimension of the Watchman device", "Watchman")
