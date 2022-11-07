69bec284a2997dff882433fb952700f5bbcbe615
Bowen Zhang <e0555834@u.nus.edu>
2022-11-07 12:17:35 +0800

    update chatbot

--- AI-Asst/chatbot.R
+++ AI-Asst/chatbot.R
@@ -1,25 +1,30 @@
 library(jsonlite)
+library(httr)
 
 
 ######## answer1 #########
-chatbot <- function(file="answers1.js", find=1) {
+chatbot <- function(file="outputs.json", find=1) {
   docs <- jsonlite::fromJSON(file)
   find <- as.numeric(find)
-  
   answers <- docs['answers'][[1]]
   if (length(answers) < find){
-    output <- "-1" #"Sorry, no more infomation available.  Please search for another device!"
+    output <- "-1" #"Sorry, no more information available.  Please search for another device!"
   }else{
     output <- answers %>% as_tibble() %>% 
       arrange(desc(score)) %>% slice(find) %>% 
       select("answer") %>% as.character()
   }
-  output
+  return (output)
 }
 
 build_chatbot <- function(device, ques, find=1){
   # connect to model file and generate json object
-  file = "answers1.js"
+  url = "http://flask:5000/"
+  body = list(question = ques, device = device)
+  resp<-GET(url, path = "prediction", params = body)
+  # t1<-content(resp, type="application/json")
+  # file = "outputs.json"
+  file = http_type(resp)
   output <- chatbot(file, find)
 }
 
